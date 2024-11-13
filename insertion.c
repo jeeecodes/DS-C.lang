bda65ca0f01a600d89af70e198247a65c2d142b1
jeeecodes <jeegeo123@gmail.com>
2024-11-13 10:43:07 +0530

    Update InsertionSort

--- insertion.c
+++ insertion.c
@@ -1,46 +1,29 @@
-#include<stdio.h>
-void main()
-{
- int n,a[100],i,j,temp,t;
- t++;
- printf("enetr the size of arrray");
- t++;
- scanf("%d",&n);
- t++;
- printf("enter theb elmnts");
- t++;
- t++;
- for(i=0;i<n;i++){
- t++;
- t++;
- scanf("%d",&a[i]);
- }
- t++;
- for(i=1;i<n;i++){
- t++;
- t++;
- temp=a[i];
- j=i-1;
- t++;
- while(j>=0 && a[j]>temp)
- {
- t++;
-  a[j+1]=a[j];
-  j--;
-  }
-  a[j+1]=temp;}
-  
-  printf("the sorted array is" );
-  t++;
-  for(i=0;i<n;i++){
-   t++;
-   t++;
-   printf("%d \t",a[i]);
-   }
-   t++;
-   printf("\n");
-   t++;
-   printf("\n the space comlexity is %d",(4*n)+(4*6));
-   t++;
-   printf("\nthe time complexity is %d",t+1);
-   }
+#include<stdio.h> 
+
+void main() {
+    int a[100], i, n, j, temp;
+
+    printf("\n Enter the size of the array: ");
+    scanf("%d", &n);
+    printf("\n Enter the elements: ");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    printf("\n entered array is \n");
+     for(i = 0; i < n; i++) {
+        printf("%d \t", a[i]);}
+        for(i=1;i<n;i++){ 
+            temp=a[i];
+            j=i-1;
+            while(j>=0 && a[j]>temp){
+                a[j+1]=a[j];
+                j--;
+
+            }
+            a[j+1]=temp;
+
+        }printf("the sorted array is:\n");
+        for(i=0;i<n;i++){
+            printf("%d \t",a[i]);
+        }
+        }
